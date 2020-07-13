cc886bd2e6377b6e14933ba80d0d0c86e71380bc
jeageun <htpy123@naver.com>
2020-07-13 12:16:30 +0900

    Web07_Login 에 LoginFiter 완성 후 테스트 페이지에서 테스트 함

--- Web07_Login_My/src/test/filter/LoginFilter.java
+++ Web07_Login_My/src/test/filter/LoginFilter.java
@@ -1,6 +1,7 @@
 package test.filter;
 
 import java.io.IOException;
+import java.net.URLEncoder;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -13,7 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebFilter({"/users/private/*"})
+@WebFilter({"/users/private/*","/test/*"}) //test폴더도 로그인 필터링 설정
 public class LoginFilter implements Filter{
 
 	@Override
@@ -35,10 +36,27 @@ public class LoginFilter implements Filter{
 			//요청의 흐름 계속 진행 시키기
 			chain.doFilter(request, response);
 		}else { //로그인 안된 상태
-			//로그인 폼으로 강제 리다일렉트 응답을 준다.
-			HttpServletResponse res =(HttpServletResponse)response;
+			/*
+			 * 	로그인 페이지로 강제로 리다일렉트 했다면 로그인성공후에 원래 가려던
+			 * 	목적지로 다시 보내야 하고
+			 * 	GET 방식 전송되는 파라미터가 있다면 파라미터 정보도 같이 가지고
+			 * 	다녀야 한다.
+			 */
+			//원래 가려던 url 정보 읽어오기
+			String url=req.getRequestURI();
+			//GET 방식 전송 파라미터를 query string 으로 얻어오기
+			String query=req.getQueryString();
+			//인코딩을 한다
+			String encodedUrl=null;
+			if(query==null) {	//전송 파라미터가 없다면
+				encodedUrl=URLEncoder.encode(url);
+			}else {
+				encodedUrl=URLEncoder.encode(url+"?"+query);
+			}
+			//로그인 폼으로 리다일렉트 이동하라고 응답
+			HttpServletResponse res=(HttpServletResponse)response;
 			String cPath=req.getContextPath();
-			res.sendRedirect(cPath+"/test/loginform.jsp");
+			res.sendRedirect(cPath+"/users/loginform.jsp?url="+encodedUrl);
 		}
 	}
 
